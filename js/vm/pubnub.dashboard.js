c2515edac859ccd3ccf8655dbc9695af844461a0
rahulkumar-aws <rahulkumar.aws@gmail.com>
2014-01-26 04:11:52 +0530

    WorldMap code changed.

--- js/vm/pubnub.dashboard.js
+++ js/vm/pubnub.dashboard.js
@@ -1,10 +1,17 @@
-var DasboardVM = function(){
-	var self = this;
-	self.name = "Rahul Kumar";
-	self.worldMapVM = new WorldMapVM();
+var DasboardVM = function () {
+    var self = this;
+    self.topChannelsViewTitle = "Top 10 channels";
+    self.trafficViewTitle = "Traffic";
+    self.topReasonViewTitle = "Top 10 Countries based on Total Message Volume";
+
+    self.topChannelsViewTitle = "Top 10 channels by device connection";
+    self.deviceTypeViewTitle = "Device Type";
+    self.topReasonViewTitle = "Top 10 countries by device connections";
+
+
+    self.worldMapVM = new WorldMapVM();
     self.messageDataVM = new MessageDataVM();
-	self.deviceDataVM = new DeviceDataVM();
-	
-	
-	
+    self.deviceDataVM = new DeviceDataVM();
+
+
 };
--- js/vm/pubnub.worldmap.dev.js
+++ js/vm/pubnub.worldmap.dev.js
@@ -5,7 +5,9 @@ var WorldMapVM = function () {
     self.worldmapProperty = {
         worldmapWidth: ko.observable(0),
         worldmapHeight: ko.observable(0),
-        projection: null
+        projection: null,
+        zoomLevel: ko.observable(1),
+        isZoomed: false
     };
 
 
@@ -66,7 +68,7 @@ var WorldMapVM = function () {
                 g.append("g").attr("id", "countries").selectAll(
                         "path").data(countries).enter().append("path").attr("id",function (d, i) {
                         return d.id;
-                    }).on("click", country_clicked)
+                    })//.on("click", country_clicked)
                     .attr("title", function (d, i) {
                         return d.properties.name;
                     })
@@ -89,101 +91,30 @@ var WorldMapVM = function () {
                             .style("opacity", 0);
                     });
 
-               // self.getPubNubTraficData();
+                // self.getPubNubTraficData();
                 // load and display the cities
 
 
             });
 
-        function get_xyz(d) {
-            var bounds = path.bounds(d);
-            var w_scale = (bounds[1][0] - bounds[0][0]) / width;
-            var h_scale = (bounds[1][1] - bounds[0][1]) / height;
-            var z = .96 / Math.max(w_scale, h_scale);
-            var x = (bounds[1][0] + bounds[0][0]) / 2;
-            var y = (bounds[1][1] + bounds[0][1]) / 2 + (height / z / 6);
-            return [x, y, z];
-        }
-
-        function country_clicked(d) {
-            g.selectAll(["#states", "#cities"]).remove();
-            state = null;
-
-            /*  if (country) {
-             g.selectAll("#" + country.id).style('display', null);
-             }*/
-            if (d && country !== d) {
-                var xyz = get_xyz(d);
-                country = d;
-
-                if (d.id == 840 || d.id == 'JPN') {
-                    d3.json("./data/us-10m.json", function (error, us) {
-                        g.append("g")
-                            .attr("id", "states")
-                            .selectAll("path")
-                            .data(topojson.feature(us, us.objects.states).features)
-                            .enter()
-                            .append("path")
-                            .attr("id", function (d) {
-                                return d.id;
-                            })
-                            .attr("class", "active")
-                            .attr("d", path)
-                        /* .on("click", state_clicked);*/
-
-                        // zoom(xyz);
-                        g.selectAll("#" + d.id).style('display', 'none');
-                    });
-                } else {
-                    //zoom(xyz);
-                }
-            }
-        }
-
-
-        // Zoom in/out buttons:
-
-
         var zm = d3.behavior.zoom().scaleExtent([1, 6]).on(
             "zoom",
             function (d) {
-
-                self.currentTranslate = d3.event.translate;
-                console.log("self.currentTranslate ", self.currentTranslate);
-                console.log("zm.scale() ", zm.scale());
 
                 g.attr("transform", "translate("
                     + d3.event.translate.join(",") + ")scale("
                     + d3.event.scale + ")")
                 if (zm.scale() != undefined && zm.scale() > 1) {
                     //d3.selectAll('.sub').attr("r",2);
-                    self.isZoom = true;
-                    self.zoomLevel(zm.scale());
+                    self.worldmapProperty.isZoomed = true;
+                    self.worldmapProperty.zoomLevel(zm.scale());
                 } else {
-                    self.isZoom = false;
-                    self.zoomLevel(1);
+                    self.worldmapProperty.isZoomed = false;
+                    self.worldmapProperty.zoomLevel(1);
                 }
 
             });
 
-
-        d3.select('#zoomIn').on('click', function () {
-            g.attr("transform", "scale(" + zm.scale() * 2 + ")");
-
-        });
-        d3.select('#zoomOut').on('click', function () {
-            if (zm.scale() > 1) {
-                g.attr("transform", "scale(" + zm.scale() * 0.5 + ")");
-            }
-
-        });
-
-        d3.select("#resetBtn").on('click', function () {
-
-            zm.scale(1);
-            zm.translate([0, 0]);
-
-        });
 
         svg.call(zm);
     };
