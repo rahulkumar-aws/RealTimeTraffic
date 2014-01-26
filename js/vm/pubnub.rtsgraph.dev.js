fdc0053251e611416020cec218892dc82ac62a23
rahulkumar-aws <rahulkumar.aws@gmail.com>
2014-01-26 16:16:50 +0530

    Network Graph Added. -2

--- js/vm/pubnub.rtsgraph.dev.js
+++ js/vm/pubnub.rtsgraph.dev.js
@@ -15,6 +15,11 @@ var RealTimeGraphVM = function () {
 
     self.topTenCountryArray = [];
     self.topTenCountryData = [];
+    self.topTenCountryByDevice = [];
+    self.deviceinfo = [];
+    self.allDevices = [];
+    self.allCountry = [];
+    self.usedDeviceData = [];
     self.totalPubMessage = ko.observable(0);
     self.totalSubMessage = ko.observable(0);
     self.pPerSecond = ko.observable(0);
@@ -67,6 +72,7 @@ var RealTimeGraphVM = function () {
                 self.rtsStatsMessages.totalHistoryMessages(0),
                 self.rtsStatsMessages.totalHistoryRequest(0);
             self.topTenCountryArray = [];
+            self.allCountry = [];
             self.totalPubMessage = ko.observable(0);
             self.totalSubMessage = ko.observable(0);
             for (var i = 0; i < currentRTSWorkingData.length; i++) {
@@ -76,7 +82,6 @@ var RealTimeGraphVM = function () {
             }
 
             if (self.topTenCountryArray.length > 0) {
-                console.log(self.topTenCountryArray);
 
                 var temp = [];
                 for (var i = 0; i < self.topTenCountryArray.length; i++) {
@@ -95,7 +100,7 @@ var RealTimeGraphVM = function () {
                     }
                     tempObj.name = countryName;
                     tempObj.value = valueCount;
-                    /*self.allCountry.push(countryName);*/
+                    self.allCountry.push(countryName);
                     temp.push(tempObj);
                 }
 
@@ -106,9 +111,91 @@ var RealTimeGraphVM = function () {
 
 
             }
+            ;
+
+
+            //Device Chart Logic
+            var devicearray = ["Chrome", "curl", "Android", "AndroidTab", "PC", "iOS", "Mac OS", "iPad"];
+            var testDevice = [];
+            for (var i = 0; i < devicearray.length; i++) {
+
+                var matcher = devicearray[i];
+                var total = 0;
+                for (var j = 0; j < self.deviceinfo.length; j++) {
+
+                    var currentdevice = self.deviceinfo[j];
+
+                    var tempObj = {};
+
+                    $.each(currentdevice, function (k, v) {
+                        if (k.indexOf(matcher) != -1) {
+                            tempObj.name = matcher;
+                            tempObj.value = v;
+                            testDevice.push(tempObj);
+                        }
+                    });
+                }
+            }
+            var finalArray = [];
+            for (var i = 0; i < testDevice.length; i++) {
+                var currentDevice = testDevice[i].name;
+                var valueCount = 0;
+                var deviceMap = {};
+                for (var j = 0; j < testDevice.length; j++) {
+                    if (currentDevice == testDevice[j].name) {
+                        valueCount = valueCount + testDevice[j].value;
+                    }
+                }
+                deviceMap.device = currentDevice;
+                deviceMap.used = valueCount;
+                self.allDevices.push(valueCount);
+                finalArray.push(deviceMap);
+            }
+            self.usedDeviceData = _.uniq(finalArray, function (device) {
+                return device.device;
+            });
+
+
+            //Top Country By Device
+            var noOfdevice = [];
+            for (var i = 0; i < self.allCountry.length; i++) {
+                var a = {};
+                a.country = self.allCountry[i];
+                a.noOfdevice = self.allDevices[i];
+                noOfdevice.push(a);
+            }
+            var temp = [];
+            for (var i = 0; i < noOfdevice.length; i++) {
+
+                var countryName = noOfdevice[i].country;
+                var deviceCount = 0;
+                var aa = {};
+                for (var j = 0; j < noOfdevice.length; j++) {
+
+                    if (countryName == noOfdevice[j].country) {
+
+                        deviceCount = deviceCount + noOfdevice[j].noOfdevice;
+
+                    }
+
+                }
+                aa.name = countryName;
+                aa.value = deviceCount;
+
+                temp.push(aa);
+            }
+
+
+            self.topTenCountryByDevice = _.uniq(temp, function (country) {
+                return country.name;
+            });
+
+
+            console.log("******** ", self.topTenCountryByDevice);
 
             self.plotTopCountriesByTotalMsgVolumeChart(self.topTenCountryData);
-
+            self.plotTopCountriesByDeviceChart(self.topTenCountryByDevice);
+            self.plotTopDeviceChart(self.usedDeviceData);
 
         }, 7 * 1000)
     };
@@ -161,6 +248,12 @@ var RealTimeGraphVM = function () {
             if (key.indexOf('geo') != -1) {
                 traverse(value, geoProcess);
             }
+
+            if (key == 'unique_user_agents') {
+
+                self.deviceinfo.push(value);
+            }
+
         };
         function geoProcess(key, value) {
             var countryMap = {};
@@ -177,7 +270,7 @@ var RealTimeGraphVM = function () {
                 self.totalPubMessage(self.totalPubMessage() + value);
             }
             if (key == 's') {
-                 self.totalSubMessage(self.totalSubMessage() + value);
+                self.totalSubMessage(self.totalSubMessage() + value);
             }
 
         };
@@ -270,7 +363,7 @@ var RealTimeGraphVM = function () {
     };
     self.plotNetworkTrafficGraph = function () {
 
-        var smoothie = new SmoothieChart({scaleSmoothing: 1, grid: {fillStyle: 'rgba(0,0,0,0.34)'},maxValueScale:1.5});
+        var smoothie = new SmoothieChart({scaleSmoothing: 1, grid: {fillStyle: 'rgba(0,0,0,0.34)'}, maxValueScale: 1.5});
 
         smoothie.streamTo(document.getElementById("message-traffic-chart"), 500);
 
@@ -281,7 +374,7 @@ var RealTimeGraphVM = function () {
 
         // Add a random value to each line every second
         setInterval(function () {
-            line1.append(new Date().getTime(),self.totalPubMessage());
+            line1.append(new Date().getTime(), self.totalPubMessage());
             line2.append(new Date().getTime(), self.totalSubMessage());
         }, 1000);
 
@@ -293,9 +386,135 @@ var RealTimeGraphVM = function () {
 
     self.plotNetworkTrafficGraph();
 
-    self.plotTopCountriesByDeviceChart = function () {
+    self.plotTopCountriesByDeviceChart = function (deviceData) {
+
+        var color = ["#5AAD2A", "#007AFF", "#E573B9", "#FFCC00", "#E30518", "#708090", "#FFFF00", "#A0522C", "#FFB6C1", "#9732CE"];
+        $("#top-ten-countries-device-div").html(" ");
+        function type(d) {
+            d.value = +d.value;
+            return d;
+        }
+
+        var data = deviceData;//self.topTenDeviceData
+        var wi = $("#top-ten-countries-device-div").width();
+        var hi = $("#top-ten-countries-device-div").height();
+        var margin = {
+            top: 20,
+            right: 20,
+            bottom: 30,
+            left: 40
+        }, width = wi - margin.left - margin.right, height = hi - margin.top
+            - margin.bottom;
+
+
+
+        var svg = d3.select("#top-ten-countries-device-div").append("svg").attr("width",
+                width+ margin.left + margin.right).attr("height",
+                height + margin.top + margin.bottom).append("g").attr(
+                "transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+        var div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+        var x = d3.scale.ordinal().rangeRoundBands([ 0, width ], .1);
+
+        var y = d3.scale.linear().range([ height , 0 ]);
+        var xAxis = d3.svg.axis().scale(x).orient("bottom");
+
+        var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
+        x.domain(data.map(function (d) {
+            return d.name;
+        }));
+
+        y.domain([ 0, d3.max(data, function (d) {
+
+            return d.value;
+        })]);
+
+        svg.append("g").attr("class", "x axis").attr("transform",
+                "translate(0," + height + ")").call(xAxis).selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.1em")
+            .attr("dy", ".14em")
+            .attr("transform", function (d) {
+                return "rotate(-25)"
+            });
+
+        svg.append("g").attr("class", "y axis").call(yAxis).append("text")
+            .attr("transform", "rotate(-90)").attr("y", 6).attr("dy",
+                ".71em").style("text-anchor", "end").text(
+                "");
+
+        svg.selectAll(".bar1").data(data).enter().append("rect").attr(
+                "class", "bar1").attr("x",function (d) {
+                return x(d.name);
+            }).attr("width", x.rangeBand()).attr("y",function (d) {
+                return y(d.value);
+            }).attr("height", function (d) {
+                return height - y(d.value);
+            })
+            .attr("fill",function (d, i) {
+                return color[i];
+            }).on("mouseover", function (d) {
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div.html(d.value + "<br/>")
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function (d) {
+                div.transition()
+                    .duration(200)
+                    .style("opacity", 0);
+            });
+
+
     };
-    self.plotTopDeviceChart = function () {
+    self.plotTopDeviceChart = function (deviceData) {
+
+        $("#pie-chart").html(" ");
+
+        var width = $("#pie-chart").width(), height = $("#pie-chart").height(), radius = Math
+            .min(width, height) / 2;
+
+
+        var data = deviceData;
+        var color = d3.scale.ordinal().range(
+            [ "#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56",
+                "#d0743c", "#ff8c00" ]);
+
+        var arc = d3.svg.arc().outerRadius(radius - 10).innerRadius(0);
+
+        var pie = d3.layout.pie().sort(null).value(function (d) {
+            return d.used;
+        });
+
+        var svg = d3.select("#pie-chart").append("svg").attr("width", width)
+            .attr("height", height).append("g").attr("transform",
+                "translate(" + width / 2 + "," + height / 2 + ")");
+
+        data.forEach(function (d) {
+            d.used = +d.used;
+        });
+
+        var g = svg.selectAll(".arc").data(pie(data)).enter().append("g")
+            .attr("class", "arc");
+
+        g.append("path").attr("d", arc).style("fill", function (d) {
+            return color(d.data.device);
+        });
+
+        g.append("text").attr("transform",function (d) {
+            return "translate(" + arc.centroid(d) + ")";
+        }).attr("dy", ".45em").style("text-anchor", "middle").text(
+            function (d) {
+                return d.data.device;
+            });
+
+
     };
 
 
